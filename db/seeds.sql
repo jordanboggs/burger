01ba26fa3e945a30b5116462749bc642748d881b
Jordan Boggs <jordan.boggs@ucdenver.edu>
2018-02-15 09:20:37 -0700

    Created schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name)
+VALUES ("Avocado Burger");
+
+INSERT INTO burgers(burger_name)
+VALUES ("Portabello Burger");
+
+INSERT INTO burgers(burger_name)
+VALUES ("Beyond Meat Burger");
