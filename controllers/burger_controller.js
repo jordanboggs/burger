2977d754544dd9df08b7638972bc92e4a4942e52
Jordan Boggs <boggsjp@gmail.com>
2018-02-18 13:03:47 -0700

    Setting up basic view

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -9,11 +9,18 @@ const router = express.Router();
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-
+  burger.getBurgers(function(data){
+    // Data is a json object { burgers: [array of data]}
+    let hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
 });
 
 router.post("/api/burgers", function(req, res) {
-
+  
 });
 
 router.put("/api/burgers/:id", function(req, res) {
