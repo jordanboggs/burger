c34467a0b1e4f9520f15aee73a41e84f8a575534
Jordan Boggs <boggsjp@gmail.com>
2018-02-18 11:39:51 -0700

    Server setup

--- server.js
+++ server.js
@@ -2,14 +2,26 @@ const express = require('express');
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
 const path = require('path');
+const exphbs = require('express-handlebars');
 
 // Sets up the Express App
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Set handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes
+const routes = require('./controllers/burger_controller.js');
+app.use(routes);
 
 // Starts the server to begin listening
 app.listen(PORT, function() {
