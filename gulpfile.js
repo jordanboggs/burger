57dc009aba4c5caafc83629483c82c98961e7308
Jordan Boggs <boggsjp@gmail.com>
2018-02-18 14:35:31 -0700

    Figured out gulp error

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,19 @@ const sass = require('gulp-sass');
 const watchSass = require('gulp-watch-sass');
 
 gulp.task('sass', () =>
-  gulp.src('src/sass/main.scss')
+  gulp.src('./src/main.scss')
   .pipe(sass({
     includePaths: ['./node_modules/']
   }))
-  .pipe(gulp.dest('public'))
+  .pipe(gulp.dest('./public'))
 );
 
 gulp.task('sass:watch', () =>
   watchSass(
-    ['src/sass/main.scss'],
+    ['./src/main.scss'],
     {includePaths: ['./node_modules/']})
   .pipe(sass({
     includePaths: ['./node_modules']
   }))
-  .pipe(gulp.dest('public'))
+  .pipe(gulp.dest('./public'))
 );
