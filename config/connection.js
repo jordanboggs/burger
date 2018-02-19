a8ff70064e097a872b117bc7c3e32d7417983702
Jordan Boggs <boggsjp@gmail.com>
2018-02-19 15:29:36 -0700

    Fixed connection issue?

--- config/connection.js
+++ config/connection.js
@@ -3,23 +3,31 @@ let connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
+  
+  connection.connect(err => {
+    if (err) {
+      console.log("Error connecting " + error.stack);
+      return;
+    }
+    console.log("Connected as id " + connection.threadId);
+  });
 }
 else {
-  const connection = mysql.createConnection({
+  connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "root",
     port: 8889,
     database: "burgers_db"
   }); 
+  
+  connection.connect(err => {
+    if (err) {
+      console.log("Error connecting " + error.stack);
+      return;
+    }
+    console.log("Connected as id " + connection.threadId);
+  });
 }
-
-connection.connect(err => {
-  if (err) {
-    console.log("Error connecting " + error.stack);
-    return;
-  }
-  console.log("Connected as id " + connection.threadId);
-});
 
 module.exports = connection;
