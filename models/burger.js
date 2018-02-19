6b2e20c2551673ff962420ae8601f280f3aebaf1
Jordan Boggs <boggsjp@gmail.com>
2018-02-19 11:50:49 -0700

    Blank entry in form will generate random word + Burger

--- models/burger.js
+++ models/burger.js
@@ -2,6 +2,7 @@
  * burger.js gets data from ORM to feed to controller
  */
 const orm = require('../config/orm.js');
+const randomWord = require('random-word');
 
 // Also inside burger.js, create the code that will call the ORM functions 
 // using burger specific input for the ORM.
@@ -15,6 +16,11 @@ const burger = {
   },
 
   addBurger: function(name, cb) {
+    if (name.length === 0) {
+      let newWord = randomWord();
+      newWord = newWord.charAt(0).toUpperCase() + newWord.slice(1);
+      name = newWord + " Burger";
+    }
     // Adds a burger to the database
     orm.insertOne(name, function(res) {
       cb(res);
