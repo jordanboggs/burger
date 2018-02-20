6b8fb8f8fd1cd1e0520e16740888e831d854fd9f
Jordan Boggs <jordan.boggs@ucdenver.edu>
2018-02-20 12:56:30 -0700

    Removed unneeded material SCSS, minified

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const watchSass = require('gulp-watch-sass');
+const cleanCSS = require('gulp-clean-css');
+const dest = require('gulp-dest');
 
 gulp.task('sass', () =>
   gulp.src('./src/**/main.scss')
@@ -19,3 +21,10 @@ gulp.task('sass:watch', () =>
   }))
   .pipe(gulp.dest('./public'))
 );
+
+gulp.task('minify-css', () => {
+  return gulp.src('./public/*.css')
+    .pipe(cleanCSS({compatibility: '*'}))
+    .pipe(dest('./', {ext: "min.css"}))
+    .pipe(gulp.dest('./public'));
+});
