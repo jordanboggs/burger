516f73b88197e35aff78c6128e32f7eb31602799
Jordan Boggs <jordan.boggs@ucdenver.edu>
2018-02-21 09:39:05 -0700

    Fixed JS issue

--- public/burgerPage.js
+++ public/burgerPage.js
@@ -1,40 +1,39 @@
-$(document).on('ready', function(){
-  $(document).on("click", ".form-submit", function(event) {
-    event.preventDefault();
-    const newName = $("#burger-name").val().trim();
+$(document).on("click", ".form-submit", function(event) {
+  event.preventDefault();
+  const newName = $("#burger-name").val().trim();
 
-    // Post the POST request
-    $.ajax("/api/burgers", {
-      type: "POST",
-      data: {
-        "name": newName
-      }
-    }).then(function() {
-      // Reload page to show new data
-      location.reload();
-    });
-  });
-
-  $(document).on("click", ".burger-item__button", function(event) {
-    const id = $(this).data("id");
-    const devouredBurger = {
-      "id": id
-    };
-    
-    $.ajax("/api/burgers/" + id, {
-      type: "PUT",
-      data: devouredBurger
-    }).then(function() {
-      // Reload page to show new data
-      location.reload();
-    });
-  });
-
-  $("#github-link").hover(function() {
-    $(this).addClass('mdc-theme--text-primary-on-dark');
-    $(this).removeClass('mdc-theme--text-secondary-on-dark');
-  }, function() {
-    $(this).removeClass('mdc-theme--text-primary-on-dark');
-    $(this).addClass('mdc-theme--text-secondary-on-dark');
+  // Post the POST request
+  $.ajax("/api/burgers", {
+    type: "POST",
+    data: {
+      "name": newName
+    }
+  }).then(function() {
+    // Reload page to show new data
+    location.reload();
   });
 });
+
+$(document).on("click", ".burger-item__button", function(event) {
+  const id = $(this).data("id");
+  const devouredBurger = {
+    "id": id
+  };
+  
+  $.ajax("/api/burgers/" + id, {
+    type: "PUT",
+    data: devouredBurger
+  }).then(function() {
+    // Reload page to show new data
+    location.reload();
+  });
+});
+
+$("#github-link").hover(function() {
+  $(this).addClass('mdc-theme--text-primary-on-dark');
+  $(this).removeClass('mdc-theme--text-secondary-on-dark');
+}, function() {
+  $(this).removeClass('mdc-theme--text-primary-on-dark');
+  $(this).addClass('mdc-theme--text-secondary-on-dark');
+});
+
